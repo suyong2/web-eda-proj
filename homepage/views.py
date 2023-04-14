e74f64847f25403f0a8a72787cd75fbcbb728e31
suyong2 <magadheera@naver.com>
2023-04-14 18:50:56 +0900

    template 추가

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 def index(request):
-    with open("titanic_EDA.html", "r", encoding='utf-8') as f:
-        data = f.read()
-    return HttpResponse(data)
+    # with open("titanic_EDA.html", "r", encoding='utf-8') as f:
+    #     data = f.read()
+    # return HttpResponse(data)
+    return render(request, 'index.html', {})
--- webproj/settings.py
+++ webproj/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'webproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'homepage', 'template'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
